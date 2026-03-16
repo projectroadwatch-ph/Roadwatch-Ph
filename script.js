0e4820f852d066054b5ebed9c9d633f465879c4c
Philippine-Roadwatch <roadwatchph379@gmail.com>
2026-03-16 16:35:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let lng = 0;
 let cachedReports = [];
 let corsFailureAlreadyLogged = false;
 
-const API_URL = "https://script.google.com/macros/s/AKfycbyuNCE_arUN97dsvk2YPnnGv1kDP-nnY1RlyipC4206kQIUHsZTH62-hAXWjn9GigjW/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxD524MsV6MrTMx-yYpLDC5FC0gdLq0JOirU89qZGMdcwJmvOyBNJCBynwQzkugpfH2/exec";
 
 
 function getReportEndpoints() {
