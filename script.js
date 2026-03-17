cde108919f010d9664c1b02ac766a4736931ad94
projectroadwatch-ph <projectroadwatch23@gmail.com>
2026-03-17 22:43:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ const LEAFLET_STYLE_SOURCES = [
   "https://cdn.jsdelivr.net/npm/leaflet@1.9.4/dist/leaflet.css"
 ];
 
-const API_URL = "https://script.google.com/macros/s/AKfycbyEw0Sa0LgYkZz6f5i_lcWku_xNzNIV2d_IrESq1RxiulSHoqP6I9Q6tC07pXqskvNL/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbx_jvX7ih_ZgDNpG7vQ6afonarXeL76dE0TPmMnk-6WRhJ7WOgg86D1zUpWkowsErad/exec";
 
 
 function buildHomepageUrl() {
